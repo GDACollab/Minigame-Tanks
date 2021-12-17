c2742808d73951bc408828f554da6c167a7251b2
timpt0261 <rchavez0215@gmail.com>
2021-12-17 15:42:49 -0800

    Camera Adjustment and Tank Turret
    I had previously made shell, haven't programmed the code, had created a turret to tell where the shell will be shot, and even adjusted the camera to have a more colorful background

---
+++ Assets/Scripts/Bullet_Spawn.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Spawn : MonoBehaviour
+{
+    GameObject GameObject;
+    public float bullet_speed = 5.0f;
+    public float range = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
