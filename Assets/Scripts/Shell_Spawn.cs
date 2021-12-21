8c04a5283cecb1e8a4e679e70759103f2c90a47e
timpt0261 <rchavez0215@gmail.com>
2021-12-20 22:33:54 -0800

    Shell projectile made, script in the works
    Finished making a projectile object in unit that uses a particle effect for the fire around the shell and a trail object as it fires. The script for spawn the object is getting an error and the firing script doesn't work as intended. Supposedly both scripts were suppose to make the tank fire the projectile when clicking the mouse button.

---
+++ Assets/Scripts/Shell_Spawn.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Spawn : MonoBehaviour
+{
+    public GameObject projetile_postion;
+    public List<GameObject> vfx = new List<GameObject>();
+
+    private GameObject effectToSpawn;
+
+   private void Start()
+    {
+        effectToSpawn = vfx[0];
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // when mouse button pressed
+        if(Input.GetMouseButton (0))
+        {
+            Spawn_vfx();
+        }
+        
+    }
+
+    private void Spawn_vfx() 
+    {
+        GameObject vfx;
+
+        if (effectToSpawn != null)
+        {
+            vfx = Instantiate(effectToSpawn, projetile_postion.transform.position, Quaternion.identity);
+
+        }
+        else
+        {
+            Debug.Log("No fire point");
+        } 
+    }
+
+ 
+
+}
