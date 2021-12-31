d214aa3e4a493307904bb3d91eb45008f3587177
David L. Carroll <93415053+ErdrickBelmont@users.noreply.github.com>
2021-12-30 19:34:11 -0800

    Main menu finished

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,9 +6,23 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // info learned from following youtube video https://www.youtube.com/watch?v=zc8ac_qUXQY
-    public void play() {
-        Debug.Log("Play");
+    public void playBlankMap() {
         SceneManager.LoadScene(1);
+    }
+
+    public void playPinPoint()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void playTileLevel()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void playTrainingMap()
+    {
+        SceneManager.LoadScene(4);
     }
 
     public void quit()
